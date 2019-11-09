820a78350d655ab46062c90edf94ea433058f521
rgalarcia <ramon@rgalarcia.com>
2019-11-09 18:45:17 +0100

    First draft of graphic design and canvas structure

--- Idefix/Form1.Designer.cs
+++ Idefix/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Idefix
 {
-    partial class Form1
+    partial class Idefix
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,51 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Idefix));
             this.label1 = new System.Windows.Forms.Label();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(64, 24);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Idefix";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 67);
+            this.button1.Location = new System.Drawing.Point(31, 70);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "Start";
+            this.button1.Text = "Read file";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(196, 70);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(94, 18);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "Information...";
             // 
-            // Form1
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Show data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 128);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Start map";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(31, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "About Idefix";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(155, -13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(2, 700);
+            this.label3.TabIndex = 7;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(199, 113);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(831, 273);
+            this.tableLayoutPanel1.TabIndex = 8;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(177, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(865, 611);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Idefix.Properties.Resources.idefix;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 199);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(83, 104);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Idefix
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(1064, 681);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Idefix";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Idefix";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +181,13 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Idefix/Form1.cs
+++ Idefix/Form1.cs
@@ -11,35 +11,93 @@ using System.Windows.Forms;
 
 namespace Idefix
 {
-    public partial class Form1 : Form
+    public partial class Idefix : Form
     {
-        public Form1()
+
+
+        public List<double[]> msgsCat10 = new List<double[]>();
+        public List<double[]> msgsCat19 = new List<double[]>();
+        public List<double[]> msgsCat20 = new List<double[]>();
+        public List<double[]> msgsCat21 = new List<double[]>();
+        public List<string[]> fspecsCat10 = new List<string[]>();
+        public List<string[]> fspecsCat19 = new List<string[]>();
+        public List<string[]> fspecsCat20 = new List<string[]>();
+        public List<string[]> fspecsCat21 = new List<string[]>();
+
+        public Idefix()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = "Idefix - an ASTERIX data analyzer";
-            label2.Text = "Press Start and choose an ASTERIX file to analyze:";
+
+
+            label2.Text = "To start, please click the 'Read file' button on the left to select an ASTERIX file to be read.\n" +
+                "You will then be able to show the file data on a table or on a map by using the controls on the left.";
+            button2.Enabled = false;
+            button3.Enabled = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.ShowDialog();
+            openFileDialog1.Filter = "ASTERIX files (*.ast)|*.ast";
 
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 Funciones funcs = new Funciones();
                 Archivo fichero = funcs.LeerArchivo(openFileDialog1.FileName);
-                List<double[]> msgsCat19 = fichero.GetMsgsCat19();
-                List<double[]> msgsCat20 = fichero.GetMsgsCat20();
-                List<string[]> fspecs19 = funcs.GetFSPEC(msgsCat19);
-                List<string[]> fspecs20 = funcs.GetFSPEC(msgsCat20);
-                string[] helou = fspecs19[7379];
-                label2.Text = "Done";
+                this.msgsCat10 = fichero.GetMsgsCat10();
+                this.msgsCat19 = fichero.GetMsgsCat19();
+                this.msgsCat20 = fichero.GetMsgsCat20();
+                this.msgsCat21 = fichero.GetMsgsCat21();
+                this.fspecsCat10 = funcs.GetFSPEC(this.msgsCat10);
+                this.fspecsCat19 = funcs.GetFSPEC(this.msgsCat19);
+                this.fspecsCat20 = funcs.GetFSPEC(this.msgsCat20);
+                this.fspecsCat21 = funcs.GetFSPEC(this.msgsCat21);
+                label2.Visible = true;
+                tableLayoutPanel1.Visible = false;
+                pictureBox2.Visible = false;
+                label2.Text = "Successfully read file " + openFileDialog1.FileName.Split('\\').Last() + "! Use buttons on the left to access file data.";
+                label2.BackColor = System.Drawing.Color.LightGreen;
+                button2.Enabled = true;
+                button3.Enabled = true;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            tableLayoutPanel1.Visible = true;
+            pictureBox2.Visible = false;
+            tableLayoutPanel1.Controls.Clear();
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 40F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Column 1" }, 0, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Column 2" }, 1, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Column 3" }, 2, 0);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Data" }, 0, 1);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Data" }, 1, 1);
+            tableLayoutPanel1.Controls.Add(new Label() { Text = "Data" }, 2, 1);
+            ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            tableLayoutPanel1.Visible = false;
+            pictureBox2.Visible = true;
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Idefix is an ASTERIX data files reader.\n\nCreated by Aleix Coma, Ramon Garcia, Isabel Montolio, and Marti Prat. Contact ramon@rgalarcia.com for more information.\n\n(C) 2019 Universitat Politecnica de Catalunya - BarcelonaTech.", "About Idefix", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
     }
 }
